de1ca2d867391e72c4476132c27c15575c31c6b9
Michael Overgaard Nielsen <mich4863@edu.campusvejle.dk>
2019-01-17 14:55:23 +0100

    Footer created

--- src/js/App.js
+++ src/js/App.js
@@ -40,10 +40,93 @@ class App {
         document.body.insertAdjacentHTML('beforeend', jumbotronHTML)
 
         /*
-        * CykelBibilotek
+        * CYKELBIBLIOTEK
         */
        SetupCycleView();
 
+       /*
+        * FOOTER 
+        */
+       const footerHTML = `
+       <!-- Footer -->
+       <footer class="page-footer pt-4 border-top border-grey mt-5">
+       
+           <!-- Footer Links -->
+           <div class="container-fluid text-center text-md-left">
+       
+             <!-- Grid row -->
+             <div class="row justify-content-center">
+       
+               <!-- Grid column -->
+               <div class="col-md-3">
+                   <h5 class="text-uppercase">Projekter & tilbud</h5>
+       
+                   <ul class="list-unstyled">
+                     <li>
+                       <a href="#">Cykelbibliotek</a>
+                     </li>
+                     <li>
+                       <a href="#">Værksted</a>
+                     </li>
+                     <li>
+                       <a href="#">Arrangementer og foredrag</a>
+                     </li>
+                     <li>
+                       <a href="#">The Good City: Udstilling</a>
+                     </li>
+                     <li>
+                       <a href="#">Undervisning</a>
+                     </li>
+                     <li>
+                       <a href="#">Rådgivning</a>
+                     </li>
+                   </ul>
+                 </div>
+
+                 <div class="col-md-3">
+                   <h5 class="text-uppercase">Om os</h5>
+
+                   <ul class="list-unstyled">
+                     <li>
+                       <a href="#">Bestyrelsen</a>
+                     </li>
+                     <li>
+                       <a href="#">Vedtægter</a>
+                     </li>
+                     <li>
+                       <a href="#"">Vilkår</a>
+                     </li>
+                   </ul>
+                 </div>
+
+                 <!-- Grid column -->
+                 <div class="col-md-3">
+                 <h5 class="text-uppercase">Om os</h5>
+
+                 <ul class="list-unstyled">
+                   <li>
+                     <span>Bicycle Innovation Lab</span>
+                   </li>
+                   <li>
+                     <span>Prags boulevard ved sportshallen Prismen</span>
+                   </li>
+                   <li>
+                     <span>Holmbladsgade 71</span>
+                   </li>
+                   <li>
+                     <span>2300 København S</span>
+                   </li>
+                 </ul>
+               </div>
+
+             </div>
+           </div>
+           <div class="footer-copyright text-center py-3">© 2017-2018:
+             <a href="#">Ligebanan</a>
+           </div>
+         </footer>     
+       `
+       document.body.insertAdjacentHTML('beforeend', footerHTML)
        
     }
 
